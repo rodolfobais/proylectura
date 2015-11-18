b0c44d33826a50cbf8ed1c7ba0ef741cedf051cb
rodolfobais <rodolfo282@gmail.com>
2015-11-18 20:19:22 -0300

    agrego la lista de libros dinamica

--- data/smarty/templates/home.php
+++ data/smarty/templates/home.php
@@ -20,16 +20,16 @@
         <title>AdminLTE 2 | Widgets</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.5 -->
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="/css/font-awesome.min.css">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="/css/ionicons.min.css">
+        <!-- Bootstrap 3.3.5 
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">-->
+        <!-- Font Awesome 
+        <link rel="stylesheet" href="/css/font-awesome.min.css">-->
+        <!-- Ionicons 
+        <link rel="stylesheet" href="/css/ionicons.min.css">-->
         <!-- Theme style -->
         <!-- AdminLTE Skins. Choose a skin from the css/skins
-             folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css">
+             folder instead of downloading all of them to reduce the load.
+        <link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css"> -->
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -83,15 +83,9 @@
                             </div><!-- /.box-header -->
                             <div class="box-body">
                                 <ul>
-                                  <li>Lorem ipsum dolor sit amet</li>
-                                  <li>Consectetur adipiscing elit</li>
-                                  <li>Integer molestie lorem at massa</li>
-                                  <li>Facilisis in pretium nisl aliquet</li>
-                                  <li>Faucibus porta lacus fringilla vel</li>
-                                  <li>Aenean sit amet erat nunc</li>
-                                  <li>Eget porttitor lorem</li>
+                                    <?php echo $listaLibros;?>
                                 </ul>
-                              </div>
+                            </div>
                         </div>
                     </div>
                     
