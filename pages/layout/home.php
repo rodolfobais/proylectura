1adc56940794afc60c6e566f93c9f060482ce325
rodolfobais <rodolfo282@gmail.com>
2015-12-07 12:46:52 -0300

    Arreglo los botones de los sliders, todos apuntaban al primero

--- pages/layout/home.php
+++ pages/layout/home.php
@@ -25,35 +25,18 @@
                             <h3 class="box-title">'.$categ->getDescrp().'</h3>
                         </div>
                         <div class="box-body">
-                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <div id="carousel-example-generic-'.$categ->getId().'" class="carousel slide" data-ride="carousel">
                                 <ol class="carousel-indicators">
                     ';
-    /*
-    $sliders = '                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-                                    <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>';*/
     $pos=0; $active = ' class="active"';
     foreach ($sliderMae as $sliderItem) {
         if($pos != 0){$active = "";}
-        $sliders .= '              <li data-target="#carousel-example-generic" data-slide-to="'.$pos++.'" '.$active.'></li>
+        $sliders .= '              <li data-target="#carousel-example-generic-'.$categ->getId().'" data-slide-to="'.$pos++.'" '.$active.'></li>
                             ';
      }
     $sliders .= '                </ol>
                                 <div class="carousel-inner">
                             ';
-    /*
-    $sliders .= '                    <div class="item active">
-                                        <img src="http://placehold.it/900x500/39CCCC/ffffff&text=I+Love+Bootstrap" alt="First slide">
-                                        <div class="carousel-caption">First Slide</div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="http://placehold.it/900x500/3c8dbc/ffffff&text=I+Love+Bootstrap" alt="Second slide">
-                                        <div class="carousel-caption">Second Slide</div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="http://placehold.it/900x500/f39c12/ffffff&text=I+Love+Bootstrap" alt="Third slide">
-                                        <div class="carousel-caption">Third Slide</div>
-                                    </div>';*/
     $pos=0; $active = ' active';
     foreach ($sliderMae as $sliderItem) {
         if($pos != 0){$active = "";}
@@ -65,10 +48,10 @@
                             ';
     }
     $sliders .= '                </div>
-                                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                <a class="left carousel-control" href="#carousel-example-generic-'.$categ->getId().'" data-slide="prev">
                                     <span class="fa fa-angle-left"></span>
                                 </a>
-                                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                <a class="right carousel-control" href="#carousel-example-generic-'.$categ->getId().'" data-slide="next">
                                     <span class="fa fa-angle-right"></span>
                                 </a>
                             </div>
