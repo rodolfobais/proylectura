846a69dc9f927504fae065667ac577fa63389e52
rodolfobais <rodolfo282@gmail.com>
2015-12-13 17:16:24 -0300

    Agrego el js de registro

--- data/smarty/templates/maestro.php
+++ data/smarty/templates/maestro.php
@@ -86,6 +86,7 @@
         <script src="{$PROJECT_REL_DIR}/js/mensaje.js"></script>
         <script src="{$PROJECT_REL_DIR}/js/compose.js"></script>
         <script src="{$PROJECT_REL_DIR}/js/clasificados.js"></script>
+        <script src="{$PROJECT_REL_DIR}/js/registro.js"></script>
         <script type="text/javascript">
             $( document ).ready(function() {
                 refreshDivs('cuerpocentro','pages/layout/home.php');
